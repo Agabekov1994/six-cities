e637d28909b8c5697ca8b189f410f19dbb6a13bd
Rimma <mudalieva@yandex.ru>
2024-02-17 01:12:45 +0400

    17.02.2024(1)

--- src/services/api.ts
+++ src/services/api.ts
@@ -11,7 +11,7 @@ const StatusCodeMapping: Record<number, boolean> = {
 
 const shouldDisplayError = (response: AxiosResponse) => !!StatusCodeMapping[response.status]
 
-const BACKEND_URL = 'https://8.react.pages.academy/six-cities';
+const BACKEND_URL = 'https://8git.react.pages.academy/six-cities';
 const REQUEST_TIMEOUT = 5000;
 
 export const createAPI = (): AxiosInstance => {
